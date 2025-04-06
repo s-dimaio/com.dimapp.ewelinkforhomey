f5a338976a3e236dfa399fe2defd2758eea30bae
Simone Di Maio <simone.dimaio77@gmail.com>
2025-04-06 18:29:32 +0200

    Initial commit

--- remove_hidden_code.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function removeHiddenCode() {
-    const input = fs.readFileSync(0, 'utf8'); // Legge dallo standard input
-    const output = input.replace(/\/\/ START_HIDE[\s\S]*?\/\/ END_HIDE/g, '');
-    process.stdout.write(output); // Scrive sullo standard output
-}
-
-removeHiddenCode();
